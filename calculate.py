3358cab18040335e4796916c3e787f2b326bccf4
Giulia Corbet <gico3590@colorado.edu>
2019-09-05 19:56:41 +0000

    create file that performs functions in math_lib file

---
+++ calculate.py
@@ -0,0 +1,10 @@
+a = int(input("Enter an integer: "))
+b = int(input("Enter another integer: "))
+
+import math_lib
+
+div_result = div(a,b)
+add_result = add(a,b)
+
+print("a/b = " + div_result)
+print("a+b = " + add_result)
